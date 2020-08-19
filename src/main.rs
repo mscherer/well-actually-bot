6d2b50c68425b49532e29045783e7f1b4316532b
Michael Scherer <misc@redhat.com>
2020-08-19 21:31:53 +0200

    Use rust fmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![forbid(unsafe_code)]
 
-use irc::client::prelude::*;
 use futures::prelude::*;
+use irc::client::prelude::*;
 use regex::Regex;
 use std::env;
 
 #[tokio::main]
-async fn main() -> irc::error::Result<()>{
+async fn main() -> irc::error::Result<()> {
     let nick = Some(match env::var("NICK") {
         Ok(val) => val,
         Err(_e) => "momopassan".to_string(),
@@ -17,14 +17,16 @@ async fn main() -> irc::error::Result<()>{
         Err(_e) => None,
     };
 
-
     let server = Some(match env::var("SERVER") {
         Ok(val) => val,
         Err(_e) => "chat.freenode.net".to_string(),
     });
 
     let irc_chans = match env::var("CHANNELS") {
-        Ok(val) => val.split(';').map(String::from).collect::<Vec<std::string::String>>(),
+        Ok(val) => val
+            .split(';')
+            .map(String::from)
+            .collect::<Vec<std::string::String>>(),
         Err(_e) => vec!["#test-misc-bot".to_string()],
     };
 
@@ -37,7 +39,6 @@ async fn main() -> irc::error::Result<()>{
         Ok(_val) => true,
         Err(_e) => false,
     };
-
 
     let re = Regex::new(r"^(?i)h+(i+|ll+o+|e+y+)\s+(guy|dude)s?").unwrap();
     let answer = "https://heyguys.cc/";
@@ -58,16 +59,13 @@ async fn main() -> irc::error::Result<()>{
     let sender = client.sender();
 
     while let Some(message) = stream.next().await.transpose()? {
-
         if debug {
             print!("{}", message)
         };
         match message.command {
             Command::PRIVMSG(ref target, ref msg) => {
                 if re.is_match(msg) {
-                    sender.send_privmsg(
-                        message.response_target().unwrap_or(target),
-                        answer)?;
+                    sender.send_privmsg(message.response_target().unwrap_or(target), answer)?;
                 }
             }
             _ => (),
